e8eea2d53941e97a887c92bbb9685d899212d796
Neville Mascarenhas <nevosial@gmail.com>
2018-08-15 23:36:33 -0700

    Update postgres.sql

--- postgres.sql
+++ postgres.sql
@@ -3,3 +3,13 @@
   - The db_session.execute(query) returns a ResultProxy object
   - The ResultProxy object is made up of RowProxy objects
   - The RowProxy object has an .items() method that returns key, value tuples of all the items in the row.
+  
+  
+  
+  
+  /*Please add ; after each select statement*/
+CREATE PROCEDURE projectList()
+BEGIN
+	select project_name, team_lead, income from Projects
+    order by internal_id;
+END
